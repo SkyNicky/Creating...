4d218ae233277680318f41ee70db25d0cac04af1
SkyNicky <nycholasffrocha@gmail.com>
2024-06-03 19:27:34 -0300

    projeto_atualizado

---
+++ AtividadeLanchoneteChakra-main/vite-project/src/Componentes/dataMenu.js
@@ -0,0 +1,169 @@
+export const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1024 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 800 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 800, min: 464 },
+      items: 4,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 4,
+    }
+  };
+  
+export const menuHamburguer = [
+    {
+      id: 1,
+      imageurl: "../src/assets/Img/ham1.jfif",
+      name: "Hambúrguer Clássico",
+      price: "R$18,99"
+  },
+  {
+      id: 2,
+      imageurl: "../src/assets/Img/hambuguer com 3 bife cheddar e bacon.jfif",
+      name: "Hambúrguer com 3 Bifes, Cheddar e Bacon",
+      price: "R$25,99"
+  },
+  {
+      id: 3,
+      imageurl: "../src/assets/Img/hambuguer simples com cebola roxa.jfif",
+      name: "Hambúrguer Simples com Cebola Roxa",
+      price: "R$15,99"
+  },
+  {
+      id: 4,
+      imageurl: "../src/assets/Img/hambuguer com dois bife cheddar e picles.jfif",
+      name: "Hambúrguer com Dois Bifes, Cheddar e Picles",
+      price: "R$22,50"
+  },
+  {
+      id: 5,
+      imageurl: "../src/assets/Img/Hambúrguer Artesanal para vender.jfif",
+      name: "Hambúrguer Artesanal",
+      price: "R$30,00"
+  },
+  {
+      id: 6,
+      imageurl: "../src/assets/Img/hamburguer com dois bifes bacon cheddar.jfif",
+      name: "Hambúrguer com Dois Bifes, Bacon e Cheddar",
+      price: "R$28,50"
+  },
+  {
+      id: 7,
+      imageurl: "../src/assets/Img/Ideias de comida para vender no delivery_ Hambúrguer gourmet.jfif",
+      name: "Hambúrguer Gourmet",
+      price: "R$35,00"
+  },
+  {
+      id: 8,
+      imageurl: "../src/assets/Img/rosquinha de hambuguer bacon ovo cheddar.jfif",
+      name: "Rosquinha de Hambúrguer com Bacon, Ovo e Cheddar",
+      price: "R$20,00"
+  }
+  ]
+export const menuHotD = [
+    {
+      id: 1,
+      imageurl: "../src/assets/Img/cachorro quente com bastante molho.jfif",
+      name: "Cachorro Quente com Bastante Molho",
+      price: "R$12,99"
+  },
+  {
+      id: 2,
+      imageurl: "../src/assets/Img/cachorro quente com molho vermelho.jfif",
+      name: "Cachorro Quente com Molho Vermelho",
+      price: "R$13,99"
+  },
+  {
+      id: 3,
+      imageurl: "../src/assets/Img/cachorro quente com palmito.jfif",
+      name: "Cachorro Quente com Palmito",
+      price: "R$14,99"
+  },
+  {
+      id: 4,
+      imageurl: "../src/assets/Img/cachorro quente simples.jfif",
+      name: "Cachorro Quente Simples",
+      price: "R$10,99"
+  },
+  {
+      id: 5,
+      imageurl: "../src/assets/Img/calabresa.jpg",
+      name: "Calabresa",
+      price: "R$11,99"
+  },
+  {
+      id: 6,
+      imageurl: "../src/assets/Img/cachorro quente com pedaços de carne.jfif",
+      name: "Cachorro Quente com Pedaços de Carne",
+      price: "R$15,99"
+  },
+  {
+      id: 7,
+      imageurl: "../src/assets/Img/dogão.jpg",
+      name: "Dogão",
+      price: "R$16,99"
+  },
+  {
+      id: 8,
+      imageurl: "../src/assets/Img/The Most Insane Hot Dogs From America's Baseball Stadiums.jfif",
+      name: "Cachorro Quente Insano",
+      price: "R$18,99"
+  }
+  ]
+  export const menuPorcao = [
+    {
+      id: 1,
+      imageurl: "../src/assets/Img/aneis de cebola.jpg",
+      name: "Aneis de Cebola",
+      price: "R$18,99"
+  },
+  {
+      id: 2,
+      imageurl: "../src/assets/Img/batata com file de peixe.jpg",
+      name: "Mix",
+      price: "R$23,50"
+  },
+  {
+      id: 3,
+      imageurl: "../src/assets/Img/camarão.jpg",
+      name: "Camarão",
+      price: "R$30,00"
+  },
+  {
+      id: 4,
+      imageurl: "../src/assets/Img/costelinha.jpg",
+      name: "Costelinha",
+      price: "R$25,75"
+  },
+  {
+      id: 5,
+      imageurl: "../src/assets/Img/coxinhas de frango.jpg",
+      name: "Coxa de frango",
+      price: "R$15,99"
+  },
+  {
+      id: 6,
+      imageurl: "../src/assets/Img/porção de bata bacon mussarela.jfif",
+      name: "Batata com bacon",
+      price: "R$20,00"
+  },
+  {
+      id: 7,
+      imageurl: "../src/assets/Img/tiras de peixe com molho.jpg",
+      name: "File de peixe",
+      price: "R$28,50"
+  },
+  {
+      id: 8,
+      imageurl: "../src/assets/Img/torresmo.jpg",
+      name: "Torresminho",
+      price: "R$18,00"
+  }
+  ]
